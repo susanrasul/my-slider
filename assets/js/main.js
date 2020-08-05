0c9f81414f2eac2806d2fa70c6d5627107b939bb
susanrasul <susaniq@code.edu.az>
2020-08-05 19:58:33 +0400

    slider added

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplaySpeed: 4000,
+    });
+});
